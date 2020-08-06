de85b46177b835606007fe8aba7e2d2df674b32b
Silas <silas.wirth@hotmail.com>
2020-08-06 19:23:16 +0200

    update
    update

---
+++ js/app.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+    .then(registration => console.log(registration))
+    .catch(error => console.log(error));
+}
